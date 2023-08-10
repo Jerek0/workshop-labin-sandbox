78379728054443b2aeda3f50220085bea3f92c04
Jérémy Minié <jeremy.minie@gmail.com>
2023-08-10 16:04:55 -0400

    Use hbs instead of liquid

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
+const handlebars = require("handlebars");
+const fs = require('fs');
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.setLiquidOptions({
-        extname: ".html",
-        root: ["views"]
-    })
+    // Register Handlebars as the template engine
+    eleventyConfig.setLibrary("hbs", handlebars);
+
+    eleventyConfig.on('eleventy.before', async ({ dir, runMode, outputMode }) => {
+        for(let prefix of ['snippets','partials']) {
+            const dir = __dirname + '/views/'+prefix+'/';
+            const filenames = fs.readdirSync(dir);
+            filenames.forEach(function (filename) {
+                var matches = /^([^.]+).hbs$/.exec(filename);
+                if (!matches) {
+                    return;
+                }
+                var name = matches[1];
+                var template = fs.readFileSync(dir + '/' + filename, 'utf8');
+                handlebars.registerPartial(prefix+'/'+name, template);
+            });
+        }
+    });
 
     eleventyConfig.addWatchTarget("./views/partials");
     eleventyConfig.addWatchTarget("./views/snippets");
@@ -10,7 +27,8 @@ module.exports = function(eleventyConfig) {
     return {
       dir: {
         input: "views/templates",
-        output: "www"
+        output: "www",
+        data: '../../data'
       }
     }
   };
